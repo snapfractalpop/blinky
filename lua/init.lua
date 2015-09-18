bbd850312aadbe29d4692bcb34dcd41e55cd589b
Matthew Kevins <mkevins@gmail.com>
2015-09-18 01:02:36 -0400

    Set pin 4 mode in init.

--- lua/init.lua
+++ lua/init.lua
@@ -1,5 +1,8 @@
 function kill() tmr.stop(0) end
 
+pin = 4
+gpio.mode(pin, gpio.OUTPUT)
+
 tmr.alarm(0, 5 * 1000, 0, function() pcall(require, 'blinky') end)
 
 print("Waiting to start server... kill() to stop.")
